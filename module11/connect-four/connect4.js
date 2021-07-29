68b71fd2ad9eee0d8169fdd9f219e9ca0977deae
LStopani <86892839+LStopani@users.noreply.github.com>
2021-07-29 09:33:53 -0600

    Add files via upload

--- module11/connect-four/connect4.js
+++ module11/connect-four/connect4.js
@@ -6,17 +6,25 @@
 // (Done)Initial set up
 const rayOfRays = [["ray0", []], ["ray1", []], ["ray2", []], ["ray3", []], ["ray4", []], ["ray5", []], ["ray6", []]];
 const rayMap = new Map(rayOfRays);
-const winCheck = new Set([1, 2, 3]);
-const DOMcolumns = [];
+let winCheck = 0
 const resetButton = document.getElementById('newGame')
 resetButton.addEventListener("click", reset)
-const cells = document.querySelectorAll('.cell')
 let tieGameCount = 0
 
+// populating the game board
 function setDOM() {
-  for (let i = 0; i <= 6; i++) {
-    DOMcolumns.push(document.getElementById(`${i}`));
-    DOMcolumns[i].addEventListener("click", placePiece);
+  for (let c = 0; c <= 6; c++) {
+    let newColumn = document.createElement("div")
+    newColumn.id = `${c}`
+    newColumn.className = "column"
+    newColumn.addEventListener("click", placePiece)
+    for (let i = 5; i >= 0; i--) {
+      let newCell = document.createElement("div")
+      newCell.id = `ray${c}_i${i}`
+      newCell.className = "cell"
+      newColumn.appendChild(newCell);
+    }
+    game.appendChild(newColumn);
   };
 };
 
@@ -38,13 +46,13 @@ function checkLocal() {
 }
 
 
-// (Done) set turn
+//  set turn
 const playerTurn = document.getElementById('whosTurn')
 playerTurn.innerHTML = "It's Red's Turn";
 let color = "red";
 
 
-// (Done) Switches the player after click
+//  Switches the player after click
 function playerSwitch() {
   if (color === "red") {
     color = "black";
@@ -58,32 +66,29 @@ function playerSwitch() {
   }
 };
 
-// // FUNCTION -reset game
+// Resets the game
 function reset() {
-  for (let i = 4; i <= winCheck.size; i++) {
-    winCheck.delete(i);
-  }
+  winCheck = false;
 
   for (let i = 0; i <= 6; i++) {
     rayMap.set(`ray${i}`, []);
   }
 
-  for (let i = 0; i < cells.length; i++) {
-    cells[i].setAttribute("class", 'cell');
-  }
+  let gameDiv = document.getElementById("game")
+  gameDiv.innerHTML = ''
   color = "red";
   tieGameCount = 0;
   setDOM();
   localStorage.setItem("savedGame", null);
 }
 
-// // FUNCTION -win
+// What happens when someone wins
 function win() {
   alert(`${color} looses!!! Can't you do anything right?!`);
 }
 
 
-// // FUNCTION (done)check number of connections
+// checks how many pieces are connected
 function checkConnect(pickedColumn, indxOrigin, cDirection, incline) {
   let origin = rayMap.get(`ray${pickedColumn}`);
   let matchCount = 0
@@ -106,23 +111,21 @@ function checkConnect(pickedColumn, indxOrigin, cDirection, incline) {
       invMatchCount++;
     };
 
-  }
-  return matchCount + invMatchCount + 1;
-}
+  };
+  let countTotal = matchCount + invMatchCount + 1
+  if (countTotal >= 4) { return 1 };
+  return 0;
+};
 
-// // FUNCTION -(Done)Check Win
+// checkes if a player won
 function checkWin(pickedColumn, indxOrigin) {
+  let horizontal = checkConnect(pickedColumn, indxOrigin, 1, 0);
+  let inclining = checkConnect(pickedColumn, indxOrigin, 1, 1);
+  let vertical = checkConnect(pickedColumn, indxOrigin, 0, 1);
+  let declining = checkConnect(pickedColumn, indxOrigin, 1, -1)
+  winCheck = horizontal + inclining + vertical + declining
 
-  // horizontal
-  winCheck.add(checkConnect(pickedColumn, indxOrigin, 1, 0))
-    // Inclining
-    .add(checkConnect(pickedColumn, indxOrigin, 1, 1))
-    // Vertical
-    .add(checkConnect(pickedColumn, indxOrigin, 0, 1))
-    // Declining
-    .add(checkConnect(pickedColumn, indxOrigin, 1, -1))
-  let winCheckSize = winCheck.size;
-  if (winCheckSize > 3) {
+  if (winCheck >= 1) {
     for (let i = 0; i <= 6; i++) {
       let shutdownColumn = document.getElementById(`${i}`)
       shutdownColumn.removeEventListener("click", placePiece)
@@ -140,7 +143,7 @@ function checkWin(pickedColumn, indxOrigin) {
   };
 }
 
-// FUNCTION - (Done)Place piece
+// placing a piece
 function placePiece(event) {
   let pickedColumn = Number(event.target.parentElement.id)
   let indxOrigin = rayMap.get(`ray${pickedColumn}`).length
@@ -157,8 +160,10 @@ function placePiece(event) {
   };
 }
 
-checkLocal();
+// going live on the session
 setDOM();
+checkLocal();
+
 
 
 
