a4560c721ff3a564588490f33cb484756455f77c
LStopani <86892839+LStopani@users.noreply.github.com>
2021-08-26 11:54:00 -0600

    Add files via upload

--- Module14_3/apis-tvmaze/tvmaze.js
+++ Module14_3/apis-tvmaze/tvmaze.js
@@ -18,17 +18,18 @@
       }
  */
 async function searchShows(query) {
-  // TODO: Make an ajax request to the searchShows api.  Remove
-  // hard coded data.
+let searchResults = await axios.get(`https://api.tvmaze.com/search/shows?q=:${$("#search-query").val()}`)
 
-  return [
-    {
-      id: 1767,
-      name: "The Bletchley Circle",
-      summary: "<p><b>The Bletchley Circle</b> follows the journey of four ordinary women with extraordinary skills that helped to end World War II.</p><p>Set in 1952, Susan, Millie, Lucy and Jean have returned to their normal lives, modestly setting aside the part they played in producing crucial intelligence, which helped the Allies to victory and shortened the war. When Susan discovers a hidden code behind an unsolved murder she is met by skepticism from the police. She quickly realises she can only begin to crack the murders and bring the culprit to justice with her former friends.</p>",
-      image: "http://static.tvmaze.com/uploads/images/medium_portrait/147/369403.jpg"
-    }
-  ]
+  // return [
+  //   {
+  //     id: 1767,
+  //     name: "The Bletchley Circle",
+  //     summary: "<p><b>The Bletchley Circle</b> follows the journey of four ordinary women with extraordinary skills that helped to end World War II.</p><p>Set in 1952, Susan, Millie, Lucy and Jean have returned to their normal lives, modestly setting aside the part they played in producing crucial intelligence, which helped the Allies to victory and shortened the war. When Susan discovers a hidden code behind an unsolved murder she is met by skepticism from the police. She quickly realises she can only begin to crack the murders and bring the culprit to justice with her former friends.</p>",
+  //     image: "http://static.tvmaze.com/uploads/images/medium_portrait/147/369403.jpg"
+  //   }
+  // ]
+
+return searchResults.data
 }
 
 
@@ -42,12 +43,16 @@ function populateShows(shows) {
   $showsList.empty();
 
   for (let show of shows) {
+
     let $item = $(
-      `<div class="col-md-6 col-lg-3 Show" data-show-id="${show.id}">
-         <div class="card" data-show-id="${show.id}">
+      `<div class="col-md-6 col-lg-3 Show" data-show-id="${show.show.id}">
+         <div class="card" data-show-id="${show.show.id}">
            <div class="card-body">
-             <h5 class="card-title">${show.name}</h5>
-             <p class="card-text">${show.summary}</p>
+           <img src = ${show.show.image.medium}
+           alt = ${'https://store-images.s-microsoft.com/image/apps.65316.13510798887490672.6e1ebb25-96c8-4504-b714-1f7cbca3c5ad.f9514a23-1eb8-4916-a18e-99b1a9817d15?mode=scale&q=90&h=300&w=300'}>
+             <h5 class="card-title">${show.show.name}</h5>
+             <p class="card-text">${show.show.summary}</p>
+             <button>Episodes</button>
            </div>
          </div>
        </div>
@@ -56,9 +61,7 @@ function populateShows(shows) {
     $showsList.append($item);
   }
 }
-
-
-/** Handle search form submission:
+/** Handle search form submission: 
  *    - hide episodes area
  *    - get list of matching shows and show in shows list
  */
@@ -72,7 +75,6 @@ $("#search-form").on("submit", async function handleSearch (evt) {
   $("#episodes-area").hide();
 
   let shows = await searchShows(query);
-
   populateShows(shows);
 });
 
@@ -84,7 +86,22 @@ $("#search-form").on("submit", async function handleSearch (evt) {
 async function getEpisodes(id) {
   // TODO: get episodes from tvmaze
   //       you can get this by making GET request to
-  //       http://api.tvmaze.com/shows/SHOW-ID-HERE/episodes
-
+      let epList = await axios.get(`http://api.tvmaze.com/shows/${id}/episodes`)
+      let episodeRay = []
+      for (episode of epList.data) {
+        epList.id
+episodeRay.push({id: episode.id, name: episode.name, season: episode.season, number: episode.number})
+      }
   // TODO: return array-of-episode-info, as described in docstring above
+  return episodeRay
 }
+
+$("#shows-list").on("click", "button", async function(e){
+  $("#episodes-list").html('')
+  let $showId = Number($(e.target.parentElement.parentElement).data("showId"))
+  const episodesList = await getEpisodes($showId)
+  $("#episodes-area").show()
+  for (let episode of episodesList){
+    $("#episodes-list").append(`<li data-id = "${episode.id}">${episode.name} (Season ${episode.season}, Episode ${episode.number})</li>`)
+  }
+})
